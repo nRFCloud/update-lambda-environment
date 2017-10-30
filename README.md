1df47fb8bc7ac56b07fda9b1c505d4de04cac937
Markus Tacker <Markus.Tacker@NordicSemi.no>
2017-10-30 13:49:59 +0100

    feat(project): initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# @nRFCloud/update-lambda-environment
+
+[![npm](https://img.shields.io/npm/v/@nrfcloud/update-lambda-environment.svg)](https://www.npmjs.com/package/@nrfcloud/update-lambda-environment)
+[![Build Status](https://travis-ci.org/nRFCloud/update-lambda-environment.svg?branch=master)](https://travis-ci.org/nRFCloud/update-lambda-environment)
+[![Greenkeeper badge](https://badges.greenkeeper.io/coderbyheart/update-lambda-environment.svg)](https://greenkeeper.io/)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![Standard - JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+Helper for updating the environment variables of a lambda.
+
+Reads the configuration from the provided package.json, and updates the
+lambdas `description` and `version` environment variable. 
+
+Usage:
+
+    npx @nRFCloud/update-lambda-environment <package.json>
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/bin/env node
+
+const aws = require('aws-sdk');
+const t = require('tcomb');
+const fs = require('fs')
+const semverRegex = require('semver-regex')
+const {blue, green, yellow} = require('colors')
+
+const lambda = new aws.Lambda();
+
+fs.readFile(process.argv[process.argv.length - 1], (err, data) => {
+  if (err) {
+    process.stderr.write(err.toString())
+    return process.exit(1)
+  }
+  const pjson = JSON.parse(data);
+  const FunctionName = t.refinement(t.String, s => s && s.length > 0, 'Non-empty String')(process.argv[process.argv.length - 2] || pjson.name)
+  const version = t.refinement(t.String, s => s && semverRegex().test(s), 'Semantic Version String')(pjson.version)
+  const Description = t.String(pjson.description)
+  lambda
+    .getFunctionConfiguration({FunctionName})
+    .promise()
+    .then(({Environment}) => {
+      const env = Environment ? Object.assign({}, Environment) : {Variables: {}}
+      env.Variables.version = version
+      return lambda
+        .updateFunctionConfiguration({FunctionName, Environment: env, Description})
+        .promise()
+        .then(() => {
+          process.stdout.write(`Updated:     ${yellow(FunctionName)}\n`)
+          process.stdout.write(`${blue('Version:    ')} ${green(version)}\n`)
+          process.stdout.write(`${blue('Description:')} ${green(Description)}\n`)
+        })
+    })
+    .catch(err => {
+      process.stderr.write(err.toString())
+      return process.exit(1)
+    })
+})
