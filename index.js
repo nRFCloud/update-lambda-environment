3928bcb8ed7197cfece4300b6a86390a9b279eb6
Markus Tacker <Markus.Tacker@NordicSemi.no>
2017-10-30 13:55:18 +0100

    chore(cs): fix codestyle

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 #!/bin/env node
 
-const aws = require('aws-sdk');
-const t = require('tcomb');
+const aws = require('aws-sdk')
+const t = require('tcomb')
 const fs = require('fs')
 const semverRegex = require('semver-regex')
 const {blue, green, yellow} = require('colors')
 
-const lambda = new aws.Lambda();
+const lambda = new aws.Lambda()
 
 fs.readFile(process.argv[process.argv.length - 1], (err, data) => {
   if (err) {
     process.stderr.write(err.toString())
     return process.exit(1)
   }
-  const pjson = JSON.parse(data);
+  const pjson = JSON.parse(data)
   const FunctionName = t.refinement(t.String, s => s && s.length > 0, 'Non-empty String')(process.argv[process.argv.length - 2] || pjson.name)
   const version = t.refinement(t.String, s => s && semverRegex().test(s), 'Semantic Version String')(pjson.version)
   const Description = t.String(pjson.description)
